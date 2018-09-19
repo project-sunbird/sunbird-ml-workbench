a7853d223bb7889688d731cb21e24463dabd4801
ALEENA K RAJ <aleenaraj6@gmail.com>
2018-09-20 00:59:17 +0530

    Issue #19 feat:Added latest version of keyword extraction operator

--- src/main/python/daggit/oplib/core/contentTagging.py
+++ src/main/python/daggit/oplib/core/contentTagging.py
@@ -2,12 +2,15 @@ import multiprocessing
 import time
 import os
 import logging
+from functools import partial
 
-from daggit.iolib.io import Pandas_Dataframe, Read_Folder
+from daggit.iolib.io import Pandas_Dataframe, Read_Folder, Pickle_Obj
 from daggit.core.base import BaseOperator
 from ..operators_registry import get_op_callable
 from ..core.contentTaggingUtils import clean_url, identify_fileType, content_to_text_conversion, contentTotext_EndtoEnd
-from ..core.contentTaggingUtils import keyword_extraction_parallel
+from ..core.contentTaggingUtils import get_tagme_longtext, pafy_text_tokens
+from ..core.contentTaggingUtils word_proc, get_words, clean_string_list,stem_lem, get_level_keywords,jaccard_with_phrase, save_obj
+
 class ContentToText(BaseOperator):
 
     @property
@@ -118,6 +121,65 @@ class KeywordExtraction(BaseOperator):
         return {"path_to_corpus": Read_Folder(self.node.outputs[0]),
                 "path_to_LogFile": Read_Folder(self.node.outputs[1])
                 }
+    def keyword_extraction_parallel(self, dir, path_to_texts, taxonomy, extract_keywords, method, filter_criteria):
+        print("*******dir*********:", dir)
+        print("***Extract keywords***:", extract_keywords)
+        print("***Filter criteria:***", filter_criteria)
+        path_to_cid_transcript = os.path.join(path_to_texts, dir, "enriched_text.txt")
+        keywords = os.path.join(path_to_texts, dir, "keywords")
+        path_to_text_tokens = os.path.join(keywords, "text_tokens")
+        path_to_tagme_tokens = os.path.join(keywords, "tagme_tokens")
+        path_to_tagme_taxonomy_intersection = os.path.join(keywords, "tagme_taxonomy_tokens")
+
+        if os.path.isfile(path_to_cid_transcript):
+            logging.info("Transcript present for cid: {0}".format(dir))
+            try:
+
+                # text_file = os.listdir(path_to_cid_transcript)[0]
+                if os.path.getsize(path_to_cid_transcript) > 0:
+                    print("Path to transcripts ", path_to_cid_transcript)
+                    logging.info("Running keyword extraction for {0}".format(path_to_cid_transcript))
+                    logging.info("---------------------------------------------")
+
+                    if extract_keywords == True and filter_criteria == "none":
+                        logging.info("Tagme keyword extraction is running for {0}".format(path_to_cid_transcript))
+                        path_to_pafy_tagme_tokens = get_tagme_longtext(path_to_cid_transcript, path_to_tagme_tokens)
+                        logging.info("Path to tagme tokens is {0}".format(path_to_pafy_tagme_tokens))
+
+                    elif extract_keywords == False:
+                        logging.info("Text tokens extraction running for {0}".format(path_to_cid_transcript))
+                        path_to_pafy_text_tokens = pafy_text_tokens(path_to_cid_transcript, path_to_text_tokens)
+                        logging.info("Path to text tokens is {0}".format(path_to_pafy_text_tokens))
+
+                    elif extract_keywords == True and filter_criteria == "taxonomy_keywords":
+                        logging.info("Tagme intersection taxonomy keyword extraction is running for {0}".format(
+                            path_to_cid_transcript))
+                        revised_content_df = pd.read_csv(taxonomy, sep=",", index_col=None)
+                        clean_keywords = map(get_words, list(revised_content_df["Keywords"]))
+                        clean_keywords = map(clean_string_list, clean_keywords)
+                        flat_list = [item.decode("utf-8").encode("ascii", "ignore") for sublist in list(clean_keywords) for item in
+                                     sublist]
+                        taxonomy_keywords_set = set([cleantext(i) for i in flat_list])
+                        path_to_pafy_tagme_tokens = get_tagme_longtext(path_to_cid_transcript, path_to_tagme_tokens)
+                        path_to_tagme_intersect_tax = tagme_taxonomy_intersection_keywords(taxonomy_keywords_set,
+                                                                                           path_to_pafy_tagme_tokens,
+                                                                                           path_to_tagme_taxonomy_intersection)
+                        logging.info("Path to tagme taxonomy intersection tokens is {0}".format(path_to_tagme_intersect_tax))
+
+                    else:
+                        logging.info("Invalid argument provided")
+
+
+                else:
+                    logging.info("The text file {0} has no contents".format(path_to_cid_transcript))
+                    print("The text file {0} has no contents".format(path_to_cid_transcript))
+
+            except:
+                print("Raise exception for {0} ".format(path_to_cid_transcript))
+                logging.info("Raise exception for {0} ".format(path_to_cid_transcript))
+        else:
+            logging.info("Transcripts doesnt exist for {0}".format(path_to_cid_transcript))
+            print("Transcripts doesnt exist for {0}".format(path_to_cid_transcript))
 
     def run(self, extract_keywords, method, filter_criteria):
         content_meta = self.inputs["content_meta"].read()
@@ -135,16 +197,12 @@ class KeywordExtraction(BaseOperator):
         logging.info('------Transcripts to keywords extraction-----')
         logging.info("Keyword extraction method used:-{0}".format(method))
 
-        if __name__ == "__main__":
-            pool = multiprocessing.Pool(processes=4)
-            keywordExtraction_partial = partial(keyword_extraction_parallel,
-                                                path_to_text=content_to_text,taxonomy=taxonomy)  # prod_x has only one argument x (y is fixed to 10)
-          
-            results = pool.map(keywordExtraction_partial, [dir for dir in os.listdir(content_to_text)])
-            pool.close()
-            pool.join()
-
-                          
-
-
+        # if __name__ == "__main__":
+        pool = multiprocessing.Pool(processes=4)
+        keywordExtraction_partial = partial(self.keyword_extraction_parallel,
+                                            path_to_texts=content_to_text, taxonomy=taxonomy, extract_keywords=extract_keywords, method=method, filter_criteria=filter_criteria)  # prod_x has only one argument x (y is fixed to 10)
+      
+        results = pool.map(keywordExtraction_partial, [dir for dir in os.listdir(content_to_text)])
+        pool.close()
+        pool.join()
 
