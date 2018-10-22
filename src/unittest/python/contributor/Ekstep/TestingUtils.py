3db83836ae52f6ccb6f65bcb6677ba8504edd81e
mayankkum <mayank@ilimi.in>
2018-10-22 16:14:18 +0530

    Issue

---
+++ src/unittest/python/contributor/Ekstep/TestingUtils.py
@@ -0,0 +1,62 @@
+import daggit 
+import difflib 
+from daggit.contrib.ekstep.operators.contentTaggingUtils import *
+
+
+
+def content_meta_features_checking(df, mandatoy_fields):
+  check = [0 if elem in list(df.columns) else 1 for elem in mandatoy_fields]
+  if sum(check) > 0:
+    return 0
+  else:
+    return 1
+
+def sentence_similarity(sentence1, sentence2, threshold):       # sentence simil
+  sentence = difflib.SequenceMatcher(lambda x: x == " ",
+                     sentence1, 
+                     sentence2) 
+  similarity_score = sentence.ratio()*100 
+  if similarity_score >= threshold: 
+    return 1
+  else:
+    return 0 
+
+def text_Extraction(url, type_of_url, id_name, path_to_save, expected_text):  
+    text_generated_path = content_to_text_conversion(url, type_of_url, id_name, path_to_save)
+    actual_text = open(text_generated_path, "r") 
+    actual_text = actual_text.read()
+    if sentence_similarity(actual_text, expected_text, 0.90) == 1:
+        return 1
+    else:
+        return 0 
+ 
+
+def intersection_lists(list_1, list_2, threshold):
+    if 1.0*(len(set(list_1) & set(list_2))/ min(len(set(list_1)),len(set(list_2)))) > threshold:
+        return 1
+    else:
+        return 0 
+
+def keyword_extraction( path_to_text, path_to_save_tagme, expected_output):
+    file_ = open(path_to_text, "r")
+    text = file_.readline()
+    if text == '':
+        return "Text is not available"
+    
+    if detect(text) != 'en':           
+        return "Non English text"
+    else: 
+        path_to_tagme_output = get_tagme_longtext(path_to_text, path_to_save_tagme)    
+        actual_output = pd.read_csv(path_to_tagme_output)
+        actual_output = list(actual_output["KEYWORDS"])
+        intersection_lists_output = intersection_lists(actual_output, expected_output,0.8)  
+        return intersection_lists_output
+
+
+def jaccard_evaluation(content_keywords, taxonomy_keywords, evaluation_criteria, threshold):
+    evaluation_score = jaccard_with_phrase(content_keywords, taxonomy_keywords)
+    if evaluation_score[evaluation_criteria] < threshold:
+        return 1          #### Jaacard value, 
+    else:
+        return 0 
+
---
+++ src/unittest/python/contributor/Ekstep/content_tagging_tests.py
@@ -0,0 +1,31 @@
+import unittest
+import sys, os
+
+
+testdir = os.path.dirname(os.path.realpath(__file__))
+srcdir = '../../../../../src/unittest/python/contributor' 
+os.chdir(os.path.join(testdir, '../../../'))
+abs_path = os.path.abspath(os.path.join(testdir, srcdir)) 
+test_case_data_location = abs_path + "/Ekstep/test_cases_data/" 
+sys.path.insert(0, abs_path)
+
+from contributor.Ekstep.TestingUtils import content_meta_features_checking
+from contributor.Ekstep.TestingUtils import text_Extraction
+from contributor.Ekstep.TestingUtils import keyword_extraction
+from contributor.Ekstep.TestingUtils import jaccard_evaluation
+
+
+
+class UnitTests(unittest.TestCase):
+	#@staticmethod
+	#def test_content_meta_features_checking():
+	#	assert(test_case_data_location + "Content_Meta_feature_checking_df_1.csv", ['artifactUrl']) == 1 
+	@staticmethod
+	def test_keyword_extraction():
+		assert keyword_extraction(test_case_data_location + "empty.txt", test_case_data_location, ["animal"])== "Text is not available"
+	@staticmethod
+	def test_text_Extraction():
+		assert text_Extraction('https://www.youtube.com/embed/ypxU42bbqWw?autoplay=1&enablejsapi=1', 'youtube', 'id_1', test_case_data_location , "plants Baje a little seed for me to sow a little app to make it grow a little help a little thought on the Beach and that is that a little son a little shower and a little better") == 1
+	@staticmethod
+	def test_jaccard_evaluation():
+	    assert jaccard_evaluation(['simple','algebraic','problem','mathematics'],['algebraic','maths'],'jaccard', 0.6 ) == 1 
