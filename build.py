c095735bf677c41c6e35a9c8de3791f445cd958b
Adarsa <adarsasprasad@gmail.com>
2019-03-06 13:01:04 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 from pybuilder.core import init, use_plugin
 
-use_plugin("python.core")
+delete_plugin("python.core")
 use_plugin("python.install_dependencies")
 use_plugin("python.distutils")
 use_plugin('copy_resources')
