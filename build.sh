166e034bcffbe5064bbc8a1887874ec1277d8e8f
S M Y <smy.altamash@gmail.com>
2019-03-13 12:46:36 +0530

    Customizing build.sh script

--- build.sh
+++ build.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
-
-# this needs to point to virtual env where build is done
-virtualenv --python=`which python3.6` mlwb_venv
-source mlwb_venv/bin/activate
-
-cd "$(dirname "$0")"
-
-pybuilderInstalled=`pip freeze | grep 'pybuilder' | wc -l`
-
-if [ $pybuilderInstalled != 1 ]
-then
-   echo "Installing pybuilder"
-   pip install pybuilder
-fi
-
+pip install pybuilder
 pyb install_dependencies clean publish
 pyb sphinx_generate_documentation
 tox
-
-rm -rf mlwb_venv/
-
-if [ ! -d "bin" ]; then
-  mkdir 'bin'
-fi
-
+mkdir -p bin/
 cp target/dist/daggit-0.5.0/dist/* bin/
-
-#rm -rf target/
