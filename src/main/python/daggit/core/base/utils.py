3deac1158dcd6d029db3610c4a4556a77a7916b5
aleenaraj <aleenar@ilimi.in>
2020-04-08 20:31:11 +0530

    Issue #SB-18784 feat: Adding changes in function and variable names

--- src/main/python/daggit/core/base/utils.py
+++ src/main/python/daggit/core/base/utils.py
@@ -3,6 +3,8 @@ import errno
 import contextlib
 import sys
 import mock
+import yaml
+import re
 
 
 def create_dir(directory):
@@ -40,3 +42,31 @@ def get_as_list(string_or_list):
         return string_or_list
     else:
         return [string_or_list]
+
+
+def parse_config(path=None, data=None, tag='!ENV'):
+    pattern = re.compile('.*?\${(\w+)}.*?')
+    loader = yaml.SafeLoader
+    loader.add_implicit_resolver(tag, pattern, None)
+
+    def constructor_env_variables(loader, node):
+        value = loader.construct_scalar(node)
+        match = pattern.findall(value)  # to find all env variables in line
+        if match:
+            full_value = value
+            for g in match:
+                full_value = full_value.replace(
+                    f'${{{g}}}', os.environ.get(g, g)
+                )
+            return full_value
+        return value
+
+    loader.add_constructor(tag, constructor_env_variables)
+
+    if path:
+        with open(path) as conf_data:
+            return yaml.load(conf_data, Loader=loader)
+    elif data:
+        return yaml.load(data, Loader=loader)
+    else:
+        raise ValueError('Either a path or data should be defined as input')
